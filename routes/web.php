5bf70703a81283f01bf8683753fdf8c51a8199c9
Muhammad Umar <39667413+umer5555@users.noreply.github.com>
2024-11-09 19:08:17 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/greeting', function () {
     return 'Hello World';
 });
 
+Route::get('/nikku', function () {
+    return 'nikkuuuu';
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
